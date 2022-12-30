0654c4bf0de3a836d296506f9ada0468a9ad8af1
alnickelzon <al.villanueva1234@gmail.com>
2022-12-30 20:51:53 +0800

    tutorial for jwt auth. created my first access token

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+
+//body parser for json
+app.use(express.json());
+
+//routes
+app.post("/login", (request, response) => {
+  const username = request.body.username;
+  const user = {
+    name: username,
+  };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  response.json({ accessToken: accessToken });
+});
+
+app.listen(4000, () => console.log("server up running on PORT 4000..."));
