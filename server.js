51b6788e784a1171380b0a2c25f7b5a63fe15b8c
alnickelzon <al.villanueva1234@gmail.com>
2023-01-16 10:18:17 +0800

    Revert "posts object. auth function to pass as middleware, what happens is it verifies the token with bearer auth type under the /posts route, sends a filtered json response"
    This reverts commit c64b8296203485986b247b91820550e41adab54e.

--- server.js
+++ server.js
@@ -6,34 +6,10 @@ const jwt = require("jsonwebtoken");
 
 // console.log(process.env);
 
-const posts = [
-  {
-    name: "Albert",
-  },
-];
-
 //body parser for json
 app.use(express.json());
 
-//auth middleware
-const authenticateToken = (request, response, next) => {
-  const authHeader = request.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) return response.status(401).send();
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
-    if (error) return response.status(403).send();
-    request.user = user;
-    next();
-  });
-};
-
 //routes
-app.get("/posts", authenticateToken, (request, response) => {
-  response.json(posts.filter((p) => p.name === request.user.name));
-});
-
 app.post("/login", (request, response) => {
   const username = request.body.username;
   const user = {
